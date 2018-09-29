bd9422884e2cecc8dbaca6fe95b635bba67e8f41
DEVASHAH <devashah12@gmail.com>
2018-09-29 18:06:26 -0400

    Starting

--- app/src/main/java/com/vibrant/disasterrecoverysupport/Signup.java
+++ app/src/main/java/com/vibrant/disasterrecoverysupport/Signup.java
@@ -3,16 +3,11 @@ package com.vibrant.disasterrecoverysupport;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
-
+        setContentView(R.layout.activity_signup);
     }
 }
